9ffe968f397f4a9d45fdf5a233cce7667c4489e5
Mykola Fatkullin <fatkullinmykola@gmail.com>
2026-04-04 01:04:41 +0300

    Updated vite config

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,4 +1,4 @@
-import { Layout } from '../Layout/Layout';
+import { Layout } from '../Layout';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from '../../modules/HomePage';
 import { FavoritesPage } from '../../modules/FavoritesPage';
